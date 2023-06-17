620eb675c9d67a5666d5391cb9ed9f688db94605
Szymon Kruk <szymon.kruk01@gmail.com>
2023-06-17 13:27:05 +0200

    Made some basic buttons on the front panel of the app

--- EmployeeManagementApp/MainWindow.xaml.cs
+++ EmployeeManagementApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace EmployeeManagementApp
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();                  //Exit from application
+        }
     }
 }
