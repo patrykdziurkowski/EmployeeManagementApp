ba1716e74c5fc2b654826af1fdfa1753db9dacfe
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-06-17 18:57:08 +0200

    Created a DepartmentRepository with GetAll and Get methods

---
+++ Infrastructure/Repositories/DepartmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using Core;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class DepartmentRepository
+    {
+        private ISQLDataAccess _dataAccess;
+        public DepartmentRepository(ISQLDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _dataAccess
+                .ExecuteSQLQuery<Department>("SELECT * FROM departments");
+        }
+
+        public Department Get(int departmentId)
+        {
+            return _dataAccess
+                .ExecuteSQLQuery<Department>($"SELECT * FROM departments WHERE department_id = {departmentId}")
+                .FirstOrDefault();
+        }
+    }
+}
