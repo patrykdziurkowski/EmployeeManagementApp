3a1ee2d26b39de82ffb3b09b40502b010bb50de1
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-06-17 16:55:49 +0200

    Created entities that recovered database data will be mapped to

---
+++ Domain/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain
+{
+    /// <summary>
+    /// The naming of these properties must match the naming inside of the database, including casing.
+    /// Property types are strictly defined to match the types in the database.
+    /// </summary>
+    public class Employee
+    {
+        public int? EMPLOYEE_ID { get; set; }
+        public string FIRST_NAME { get; set; }
+        public string LAST_NAME { get; set; }
+        public string EMAIL { get; set; }
+        public string PHONE_NUMBER { get; set; }
+        public DateTime? HIRE_DATE { get; set; }
+        public string JOB_ID { get; set; }
+        public double? SALARY { get; set; }
+        public Single? COMMISSION_PCT { get; set; }
+        public int? MANAGER_ID { get; set; }
+        public Int16? DEPARTMENT_ID { get; set; }
+    }
+}
