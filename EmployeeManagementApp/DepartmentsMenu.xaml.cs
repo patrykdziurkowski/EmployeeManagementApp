aeb1dd4e03ba522e9ff1ffe40e7bd5ee1439fcac
Szymon Kruk <szymon.kruk01@gmail.com>
2023-06-18 16:00:52 +0200

    Added Drag and drop effect to Departments Table

--- EmployeeManagementApp/DepartmentsMenu.xaml.cs
+++ EmployeeManagementApp/DepartmentsMenu.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace EmployeeManagementApp
 {
@@ -20,23 +24,99 @@ namespace EmployeeManagementApp
     /// </summary>
     public partial class DepartmentsMenu : Page
     {
+        public delegate Point GetPosition(IInputElement element);
+        int rowIndex = -1;
+
+        private List<DepartmentsTableObject> tableDepartmentsData = new List<DepartmentsTableObject>();
+
         public DepartmentsMenu()
         {
             InitializeComponent();
 
-            List<DepartmentsTableObject> tableDepartmentsData = new List<DepartmentsTableObject>();
-
-            //Add objects
             tableDepartmentsData.Add(new DepartmentsTableObject() { Id = 1, Name = "Jan", Surname = "Jowalski", Role = "AC_MGR", Department = DepartmentsTableObject.getDepartmentName(10) });
+            tableDepartmentsData.Add(new DepartmentsTableObject() { Id = 2, Name = "Adam", Surname = "Nowa", Role = "IT_MGR", Department = DepartmentsTableObject.getDepartmentName(60) });
+            tableDepartmentsData.Add(new DepartmentsTableObject() { Id = 3, Name = "Magdam", Surname = "Nowak", Role = "ACC", Department = DepartmentsTableObject.getDepartmentName(90) });
 
             SalariesTable.ItemsSource = tableDepartmentsData;
         }
+        private object draggedItem;
+        private Point startPoint;
+
+        private void SalariesTable_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(null);
+            draggedItem = FindDataGridRow(e.OriginalSource as DependencyObject)?.Item;
+        }
+
+        private void SalariesTable_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && draggedItem != null)
+            {
+                Point position = e.GetPosition(null);
+                if (Math.Abs(position.X - startPoint.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                    Math.Abs(position.Y - startPoint.Y) > SystemParameters.MinimumVerticalDragDistance)
+                {
+                    DragDrop.DoDragDrop(SalariesTable, draggedItem, DragDropEffects.Move);
+                    draggedItem = null;
+                }
+            }
+        }
+        private void SalariesTable_Drop(object sender, DragEventArgs e)
+        {
+            var targetCanvas = e.Source as Canvas;
+            if (targetCanvas != null)
+            {
+                var targetDepartment = targetCanvas.Tag.ToString();
+                var droppedItem = e.Data.GetData(typeof(DepartmentsTableObject)) as DepartmentsTableObject;
+
+                if (droppedItem != null)
+                {
+                    droppedItem.Department = targetDepartment;
+                }
+            }
+        }
+
+        private void Canvas_Drop(object sender, DragEventArgs e)
+        {
+            var targetCanvas = sender as Canvas;
+            if (targetCanvas != null)
+            {
+                var targetDepartment = targetCanvas.Tag.ToString();
+                var droppedItem = e.Data.GetData(typeof(DepartmentsTableObject)) as DepartmentsTableObject;
+
+                if (droppedItem != null)
+                {
+                    droppedItem.Department = targetDepartment;
+
+                    SalariesTable.Items.Refresh();
+                }
+            }
+        }
+
+        private static T FindVisualParent<T>(DependencyObject obj) where T : DependencyObject
+        {
+            while (obj != null)
+            {
+                if (obj is T parent)
+                    return parent;
+                obj = VisualTreeHelper.GetParent(obj);
+            }
+            return null;
+        }
+
+        private static DataGridRow FindDataGridRow(DependencyObject obj)
+        {
+            return FindVisualParent<DataGridRow>(obj);
+        }
+
 
         private void ReturnToMainMenu(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new MainMenu());
         }
     }
+
+
 
     public class DepartmentsTableObject
     {
