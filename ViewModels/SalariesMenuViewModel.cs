ce8dc7ca50a21eb6345733fab2946853c64f3e9b
Szymon Kruk <szymon.kruk01@gmail.com>
2023-06-18 17:14:21 +0200

    Added importing data from database in Salaries table

---
+++ ViewModels/SalariesMenuViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Core;
+using Domain;
+using Infrastructure;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class SalariesMenuViewModel : INotifyPropertyChanged
+    {
+        private LoginViewModel _loginViewModel;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private ObservableCollection<SalaryViewModel> _salaries;
+
+        public ObservableCollection<SalaryViewModel> Salaries
+        {
+            get
+            {
+                return _salaries;
+            }
+            set
+            {
+                _salaries = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SalariesMenuViewModel()
+        {
+            _loginViewModel = LoginViewModel.GetInstance();
+            ConnectionStringProvider provider = new ConnectionStringProvider();
+            string connectionString = provider
+                .GetConnectionString(_loginViewModel.UserName, _loginViewModel.Password);
+            EmployeeRepository employeeRepository = new(new OracleSQLDataAccess(connectionString));
+
+            _salaries = new ObservableCollection<SalaryViewModel>();
+            List<SalaryViewModel> salaryViewModels = SalaryViewModel
+                .ToListOfSalaryViewModel(employeeRepository
+                .GetAll());
+            ObservableCollection<SalaryViewModel> salaries = new ObservableCollection<SalaryViewModel>(salaryViewModels);
+            _salaries = salaries;
+            _salaries.CollectionChanged += Salaries_CollectionChanged;
+        }
+
+        private void Salaries_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Salaries"));
+        }
+    }
+}
+
+
---
+++ ViewModels/SalaryViewModel.cs
@@ -0,0 +1,123 @@
+﻿using Core;
+using Domain;
+using Infrastructure;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class SalaryViewModel : INotifyPropertyChanged
+    {
+        private int? _employeeId;
+
+        public int? EmployeeId
+        {
+            get
+            {
+                return _employeeId;
+            }
+            set
+            {
+                _employeeId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _jobId;
+        public string JobId
+        {
+            get
+            {
+                return _jobId;
+            }
+            set
+            {
+                _jobId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double? _salary;
+        public double? Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                _salary = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SalaryViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public static List<SalaryViewModel> ToListOfSalaryViewModel(IEnumerable<Employee> salaries)
+        {
+            List<SalaryViewModel> result = new List<SalaryViewModel>();
+
+            foreach (Employee employee in salaries)
+            {
+                SalaryViewModel salaryViewModel = new()
+                {
+                    EmployeeId = employee.EMPLOYEE_ID,
+                    FirstName = employee.FIRST_NAME,
+                    LastName = employee.LAST_NAME,
+                    JobId = employee.JOB_ID,
+                    Salary = employee.SALARY,
+                };
+                result.Add(salaryViewModel);
+            }
+
+            return result;
+        }
+    }
+}
+
+
+
