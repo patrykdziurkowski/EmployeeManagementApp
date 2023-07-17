b9a4c5d7535e377b1a4ae6d16a9d2b41f2a90bf3
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-07-17 13:15:06 +0200

    Added tests for CanExecute method of UpdateEmployeeCommand

---
+++ Tests/UpdateEmployeeCommandTests.cs
@@ -0,0 +1,113 @@
+﻿using BusinessLogic;
+using BusinessLogic.Commands;
+using BusinessLogic.Validators;
+using BusinessLogic.ViewModels;
+using DataAccess;
+using DataAccess.Models;
+using DataAccess.Repositories;
+using FluentResults;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+#pragma warning disable CS1998
+    public class UpdateEmployeeCommandTests
+    {
+        private UpdateEmployeeCommand _subject;
+
+        private EmployeeValidator _employeeValidator;
+
+        private Mock<IDateProvider> _mockDateProvider;
+        private Mock<JobHistoryRepository> _mockJobHistoryRepository;
+        private Mock<JobRepository> _mockJobRepository;
+        private Mock<EmployeeRepository> _mockEmployeeRepository;
+        private Mock<EmployeesMenuViewModel> _mockViewModel;
+
+        public UpdateEmployeeCommandTests()
+        {
+            _employeeValidator = new();
+
+            Mock<ISQLDataAccess> mockDataAccess = new();
+            _mockJobHistoryRepository = new(mockDataAccess.Object);
+            _mockJobRepository = new(mockDataAccess.Object);
+            _mockEmployeeRepository = new(mockDataAccess.Object);
+
+            _mockDateProvider = new();
+
+            _mockViewModel = new(_mockEmployeeRepository.Object,
+                                _mockJobRepository.Object,
+                                _mockJobHistoryRepository.Object,
+                                _employeeValidator,
+                                _mockDateProvider.Object);
+
+            _subject = new(_mockViewModel.Object,
+                _mockEmployeeRepository.Object,
+                _employeeValidator,
+                _mockDateProvider.Object,
+                _mockJobHistoryRepository.Object);
+        }
+
+        [Fact]
+        public async Task CanExecute_GivenValidEmployee_ReturnsTrue()
+        {
+            //Arrange
+            EmployeeViewModel validEmployee = new()
+            {
+                EmployeeId = 100,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "JSMITH",
+                PhoneNumber = "111 111 1111",
+                HireDate = DateOnly.MaxValue,
+                JobId = "ST_CLERK"
+            };
+            _mockViewModel.Object.UpdatedEmployee = validEmployee;
+
+            //Act
+            bool canExecute = _subject.CanExecute(null);
+
+            //Assert
+            Assert.True(canExecute);
+        }
+
+        [Fact]
+        public async Task CanExecute_GivenInvalidEmployee_ReturnsFalse()
+        {
+            //Arrange
+            EmployeeViewModel invalidEmployee = new();
+            _mockViewModel.Object.UpdatedEmployee = invalidEmployee;
+
+            //Act
+            bool canExecute = _subject.CanExecute(null);
+
+            //Assert
+            Assert.False(canExecute);
+        }
+
+        [Fact]
+        public async Task CanExecute_GivenInvalidEmployee_SetsFailMessageAndIndicator()
+        {
+            //Arrange
+            EmployeeViewModel invalidEmployee = new();
+            _mockViewModel.Object.UpdatedEmployee = invalidEmployee;
+            _mockViewModel.Object.CommandFailMessage = null;
+            _mockViewModel.Object.IsLastCommandSuccessful = true;
+
+            //Act
+            _subject.CanExecute(null);
+
+            //Assert
+            Assert.False(_mockViewModel.Object.IsLastCommandSuccessful);
+            Assert.NotNull(_mockViewModel.Object.CommandFailMessage);
+        }
+
+
+    }
+#pragma warning restore CS1998
+}
