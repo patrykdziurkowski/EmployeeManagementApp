6e97f8d70eb517699af0bcd43be5a7f08dc761bc
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-07-24 14:54:07 +0200

    Updated the employeeValidator validation rules to reflect the database rules

--- App/BusinessLogic/Validators/EmployeeValidator.cs
+++ App/BusinessLogic/Validators/EmployeeValidator.cs
@@ -7,26 +7,50 @@ namespace BusinessLogic.Validators
     {
         public EmployeeValidator()
         {
+            int salesDepartmentId = 80;
+
             RuleFor(x => x.EmployeeId)
-                .NotEmpty();
+                .NotEmpty()
+                .GreaterThanOrEqualTo(0);
 
             RuleFor(x => x.FirstName)
-                .NotEmpty();
+                .MaximumLength(20);
 
             RuleFor(x => x.LastName)
-                .NotEmpty();
+                .NotEmpty()
+                .MaximumLength(25);
 
             RuleFor(x => x.Email)
-                .NotEmpty();
+                .NotEmpty()
+                .MaximumLength(25);
 
             RuleFor(x => x.PhoneNumber)
-                .NotEmpty();
+                .MaximumLength(20);
 
             RuleFor(x => x.HireDate)
                 .NotEmpty();
 
             RuleFor(x => x.JobId)
-                .NotEmpty();
+                .NotEmpty()
+                .MaximumLength(10);
+
+            RuleFor(x => x.Salary)
+                .GreaterThan(0)
+                .Unless(x => x.Salary is null);
+
+            RuleFor(x => x.CommissionPct)
+                .Null()
+                .Unless(x => x.DepartmentId == salesDepartmentId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.ManagerId)
+                .GreaterThan(0)
+                .Unless(x => x.ManagerId is null);
+
+            RuleFor(x => x.DepartmentId)
+                .GreaterThan((short)1)
+                .When(x => x.DepartmentId.HasValue);
+
         }
     }
 }
