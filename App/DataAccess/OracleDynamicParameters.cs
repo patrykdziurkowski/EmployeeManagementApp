950d5bf7a7bde0ac9c395917ca591933184cd489
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2023-08-07 12:32:00 +0200

    Added a class that lets us use Oracle's parameters rather than just the general ADO.NET types. This is neccessary for us to use oracle's refcursors which are used to return data from a stored procedure.

---
+++ App/DataAccess/OracleDynamicParameters.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Dapper.SqlMapper;
+
+namespace DataAccess
+{
+    public class OracleDynamicParameters : IDynamicParameters
+    {
+        private readonly DynamicParameters dynamicParameters = new();
+        private readonly List<OracleParameter> oracleParameters = new();
+
+        public void Add(string name, object? value = null, DbType? dbType = null, ParameterDirection? direction = null, int? size = null)
+        {
+            dynamicParameters.Add(name, value, dbType, direction, size);
+        }
+
+        public void Add(string name, OracleDbType oracleDbType, ParameterDirection direction)
+        {
+            OracleParameter oracleParameter = new(name, oracleDbType, direction);
+            oracleParameters.Add(oracleParameter);
+        }
+
+        public void AddParameters(IDbCommand command, Identity identity)
+        {
+            ((IDynamicParameters)dynamicParameters).AddParameters(command, identity);
+
+            OracleCommand oracleCommand = (OracleCommand)command;
+            oracleCommand.Parameters.AddRange(oracleParameters.ToArray());
+        }
+    }
+}
